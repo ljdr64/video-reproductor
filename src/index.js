7a3e738fa5fc09c7070b087e2eea3f0dcc6c22ab
Laureano Rodriguez <laure.jd0101@gmail.com>
2023-04-07 03:34:56 -0300

    Finalizacion del proyecto Video-Player

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const video = document.querySelector('#video');
+const btnPlay = document.querySelector('#btn-play');
+const btnPause = document.querySelector('#btn-pause');
+const btnMute = document.querySelector('#btn-mute');
+
+const accionPlay = () => { video.play(); };
+const accionPause = () => { video.pause(); };
+const accionMute = () => { video.muted = !video.muted };
+
+btnPlay.addEventListener('click', accionPlay);
+btnPause.addEventListener('click', accionPause);
+btnMute.addEventListener('click', accionMute);
